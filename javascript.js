03bbe3132a6eac9669adf5a9f9c997ba37400ca9
mtusek28 <mtusek28@4j.lane.edu>
2016-01-18 03:27:31 -0800

    Created a template website
    PortFolio for Trans Speak

---
+++ javascript.js
@@ -0,0 +1,19 @@
+var alwaysVar = false;
+
+function init() 
+{ 
+	$('.thumbnail').mouseover(function() {
+	   var id = ('#' + this.id);
+	   var alwaysVar = true;
+	   
+	   $(id).animate( {opacity: .5}, {always: alwaysVar}  );
+
+	});
+
+	$('.thumbnail').mouseleave(function() {
+		var id = ('#' + this.id);
+	   var alwaysVar = false;
+	   
+	   $(id).animate( {opacity: 1}, {always: alwaysVar}  );
+	});
+}
