82d5628ff84fcac446c5b4509c5c6098c4d3de7d
PrincessHugz <HiddenMuze@gmail.com>
2016-01-18 07:28:59 -0800

    Fixed devices, navbar
    Works on all devices

--- javascript.js
+++ javascript.js
@@ -1,19 +1,30 @@
-var alwaysVar = false;
+var alwaysVar1 = false;
+var alwaysVar2 = false;
 
 function init() 
 { 
 	$('.thumbnail').mouseover(function() {
 	   var id = ('#' + this.id);
-	   var alwaysVar = true;
+	   var alwaysVar1 = true;
 	   
-	   $(id).animate( {opacity: .5}, {always: alwaysVar}  );
+	   $(id).animate( {opacity: .5}, {always: alwaysVar1}  );
 
 	});
 
 	$('.thumbnail').mouseleave(function() {
-		var id = ('#' + this.id);
-	   var alwaysVar = false;
+	   var alwaysVar1 = false;
+	   var id = ('#' + this.id);
 	   
-	   $(id).animate( {opacity: 1}, {always: alwaysVar}  );
+	   $(id).animate( {opacity: 1}, {always: alwaysVar1}  );
 	});
+
+	$('.text').mouseover(function() {
+		var id = ('#' + this.id);
+		var alwaysVar2 = true;
+
+	});
+
+	$('.text').mouseleave(function() {
+		var alwaysVar2 = false;
+	})
 }
